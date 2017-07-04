26d217c2b58f8600795a4e54b19fcccec11b8a58
Vsoundharya <soundhvijay97@gmail.com>
2017-07-04 17:13:55 +0530

    Create Nloop.java

---
+++ Nloop.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Nloop{
+public static void main(String args[]){
+int n,sum=0;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(int i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+}
+}
