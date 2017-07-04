653799a804e0d806cb8ab15a0d438470adacc80b
Vsoundharya <soundhvijay97@gmail.com>
2017-07-04 15:26:33 +0530

    Update Large.java

--- Large.java
+++ Large.java
@@ -3,14 +3,17 @@ import java.util.*;
 public class Large{
 public static void main(String arg[]){
 int a,b,c;
-Scanner sc=new Scanner(System.in);
-if(a<b&&a<c)
+  Scanner sc=new Scanner(System.in);
+  a=sc.nextInt();
+  b=sc.nextInt();
+  c=sc.nextInt();
+if(a>b&&a>c)
 {
 System.out.println("a is greater number");
 }
-else if(b<c&&b<a)
+else if(b>c&&b>a)
 {
-System.out.println("b is greater number"):
+System.out.println("b is greater number");
 }
 else
 {
