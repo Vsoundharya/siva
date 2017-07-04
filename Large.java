e2bfdd9ac2220175cb538b808d1d6763d4d4b8ab
Vsoundharya <soundhvijay97@gmail.com>
2017-07-04 15:23:40 +0530

    Create Large.java

---
+++ Large.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Large{
+public static void main(String arg[]){
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+if(a<b&&a<c)
+{
+System.out.println("a is greater number");
+}
+else if(b<c&&b<a)
+{
+System.out.println("b is greater number"):
+}
+else
+{
+System.out.println("c is greater number");
+}
+}
+}
