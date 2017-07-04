64ded2153b4480093916875558100c14a630038b
Vsoundharya <soundhvijay97@gmail.com>
2017-07-04 18:07:53 +0530

    Create Pow.java

---
+++ Pow.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Pow{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+int a=sc.nextInt();
+System.out.println("Enter the power of a number");
+int b=sc.nextInt();
+System.out.println(Math.pow(a,b));
+
+}
+}
