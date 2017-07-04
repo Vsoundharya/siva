bbaf19c9746f4fe2e5f17332c2d930fa54e5a7d6
Vsoundharya <soundhvijay97@gmail.com>
2017-07-04 15:46:49 +0530

    Create Hello5.java

---
+++ Hello5.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Hello5{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int i;
+for(i=0;i<5;i++)
+{
+System.out.println("Hello");
+}
+}
+}
