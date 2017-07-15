6bf9250e46ad9df797e7b44c3042d747edc62a78
Vsoundharya <soundhvijay97@gmail.com>
2017-07-15 14:26:12 +0530

    Create Hcf.java

---
+++ Hcf.java
@@ -0,0 +1,28 @@
+# siva
+  import java.io.*;
+  import java.util.*;
+  public class Hcf{
+    
+    public static void main(String args[])
+    {
+    int hcf=0;
+    
+     Scanner sc=new Scanner(System.in);
+     int a=sc.nextInt();
+     int b=sc.nextInt();
+     int min=Math.min(a,b);
+     for(int i=min;i>=1;i--)
+     {  
+       if(a%1==0 && b%2==0)
+       {
+         hcf=i;
+         break;
+         }  }
+        
+           System.out.println(hcf);
+           }
+           }
+
+
+
+
