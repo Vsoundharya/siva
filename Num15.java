dc08ac2355ec36eb3acf884386e2c72968321391
Vsoundharya <soundhvijay97@gmail.com>
2017-07-16 10:41:13 +0530

    Create Num15.java

---
+++ Num15.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Num15{
+  public static void main(String args[]){
+    int a=0,b=0;
+    for(int i=1;i<=15;i++)
+    {
+      a+=i;
+      }
+      System.out.println(a);
+      for(int j=15;j<=45;j+=2)
+      {
+        b+=j;
+        }
+        System.out.println(b);
+        }
+        }
