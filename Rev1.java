0efca66ed5b58cf209adab94672c6a2a83ed32bf
Vsoundharya <soundhvijay97@gmail.com>
2017-07-16 12:29:51 +0530

    Create Rev1.java

---
+++ Rev1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Rev{
+  public static void main(String args[]){
+    String s="Dhoni";
+    StringBuilder str = new StringBuilder(s);
+    System.out.println("string = " + str);
+    System.out.println("reverse = " + str.reverse());
+  }
+}
