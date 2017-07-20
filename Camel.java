7727ca36a05af9fa9956a17b28bd8139ca8301f2
Vsoundharya <soundhvijay97@gmail.com>
2017-07-20 09:24:27 +0530

    Create Camel.java

---
+++ Camel.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class CamelCase{
+public static void main(String args[]){
+String str="MahendraSinghDhoni";
+String res="";
+for(int i=0;i<str.length();i++)
+{
+Character ch=str.charAt(i);
+if(Character.isUpperCase(ch))
+{
+res+=" "+Character.toUpperCase(ch);
+}
+else
+{
+res+=ch;
+}
+}
+System.out.println(res);
+}
+}
